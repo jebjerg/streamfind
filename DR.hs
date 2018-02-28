dcf9f938fe1f0bd420fabf1bbe6a62c0a1ccc473
casper <casper@localhost>
2018-02-28 21:16:03 +0100

    DR JSON parsing cleaned up: added decodeEpisodes (either), fewer intermediary types

--- DR.hs
+++ DR.hs
@@ -5,8 +5,10 @@ module DR where
 import           Common               (eitherGetWith, extractKey, prefixError,
                                        responseBody')
 import           Control.Lens         ((&), (.~))
-import           Data.Aeson           (FromJSON, eitherDecode, parseJSON,
-                                       withObject)
+import           Data.Aeson           ((.:), FromJSON, eitherDecode, parseJSON,
+                                       withObject, withArray, Value)
+import           Data.Aeson.Types     (parseEither)
+import           Data.Vector          (toList)
 import           Data.ByteString.Lazy (ByteString)
 import           Data.List            (map)
 import qualified Network.Wreq         as WWW
@@ -14,33 +16,32 @@ import           Types                (EitherWWWResponse, Error, Query,
                                        Response, Result (..), ToResult,
                                        toResult)
 
-newtype DRResult = DRResult
-  { episodes :: [DREpisode]
-  } deriving (Show)
-
-instance FromJSON DRResult where
-  parseJSON =
-    withObject "dr" $ \o -> do
-      eps <- extractKey "Episodes" o
-      return $ DRResult eps
-
 data DREpisode = DREpisode
   { programcardTitle      :: String
   , programcardSlug       :: String
   , hasPublicPrimaryAsset :: Bool
   } deriving (Show)
 
-instance FromJSON DREpisode where
-  parseJSON =
-    withObject "ep" $ \o -> do
-      title <- extractKey "ProgramcardTitle" o
-      slug <- extractKey "ProgramcardSlug" o
-      public <- extractKey "HasPublicPrimaryAsset" o
-      return $ DREpisode title slug public
-
 instance ToResult DREpisode where
   toResult ep =
     Result (programcardTitle ep) Nothing Nothing Nothing (hasPublicPrimaryAsset ep) "DR"
+
+instance FromJSON DREpisode where
+  parseJSON =
+    withObject "ep" $ \o -> do
+      title <- o .: "ProgramcardTitle"
+      slug <- o .: "ProgramcardSlug"
+      public <- o .: "HasPublicPrimaryAsset"
+      return $ DREpisode title slug public
+
+decodeEpisodes :: ByteString -> Either String [DREpisode]
+decodeEpisodes s = eitherDecode s >>= root >>= eps
+  where
+    root :: (FromJSON a) => Value -> Either String a
+    root = parseEither $ withObject "root" (.: "Episodes")
+
+    eps :: (FromJSON a) => Value -> Either String [a]
+    eps = parseEither $ withArray "eps" $ mapM parseJSON . toList
 
 -- "https://www.dr.dk/mu-online/api/1.3/list/view/quicksearch/" ++ query ++ "?limitprograms=" ++ show nPrograms ++ "%26limitepisodes=" ++ show nEpisodes ++ "%26orderBy=PrimaryBroadcastDay%26orderDescending=true"
 drApiUrl :: Integer -> Integer -> Query -> String
@@ -51,9 +52,9 @@ decodeDR (Left err) = Left err
 decodeDR (Right response) =
   case drResult of
     Left x         -> Left $ "DR error:\n" ++ x
-    Right response -> Right . map toResult $ episodes response
+    Right episodes -> Right . map toResult $ episodes
   where
-    drResult = eitherDecode . responseBody' $ response :: Either Error DRResult
+    drResult = decodeEpisodes . responseBody' $ response
 
 searchDR :: Query -> IO Response
 searchDR q = decodeDR . prefixError "DR:\n" <$> drData
