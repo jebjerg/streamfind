d4a6fa27909b54700820949ba1253a3734e009d7
casper <casper@localhost>
2018-04-06 17:03:01 +0200

    TV2: use monadic instance of Either

--- src/StreamFind/Providers/TV2.hs
+++ src/StreamFind/Providers/TV2.hs
@@ -2,50 +2,48 @@
 
 module StreamFind.Providers.TV2 where
 
-import           StreamFind.Common          (eitherGetWith, unpackResponse,
-                                             urlEncode')
-import           StreamFind.Types           (Error, Query, Response,
-                                             Result (..))
+import           StreamFind.Common    (eitherGetWith, prefixError,
+                                       responseBody', urlEncode')
+import           StreamFind.Types     (Error, Query, Response, Result (..))
 
-import           Control.Lens               ((&), (.~))
-import           Data.Aeson                 (Value, eitherDecode, withArray,
-                                             withObject, (.:))
-import           Data.Aeson.Types           (Parser, parseEither)
-import qualified Data.ByteString.Lazy.Char8 as LBSC
-import           Data.Vector                (toList)
-import qualified Network.Wreq               as WWW
+import           Control.Lens         ((&), (.~))
+import           Data.Aeson           (Value, eitherDecode, withArray,
+                                       withObject, (.:))
+import           Data.Aeson.Types     (Parser, parseEither)
+import           Data.ByteString.Lazy (ByteString)
+import           Data.Vector          (toList)
+import qualified Network.Wreq         as WWW
 
 providerName = "TV2"
 
+decodeTV2 :: ByteString -> Either Error [Result]
+decodeTV2 resp = eitherDecode resp >>= hits
+  where
+    hits :: Value -> Either Error [Result]
+    hits = parseEither $ withArray "suggestions" $ \s -> mapM hit (toList s)
+    hit :: Value -> Parser Result
+    hit =
+      withObject "hit" $ \o -> do
+        title <- o .: "label"
+        slug <- o .: "url"
+        return $
+          Result
+            title
+            Nothing
+            (Just $ "https://play.tv2.dk" ++ slug)
+            Nothing
+            True
+            providerName
+
 searchTV2 :: Query -> IO Response
 searchTV2 q = do
-  tv2Data <- tv2Resp
-  return $
-    case tv2Data of
-      Left e -> Left $ providerName ++ ":\n" ++ e
-      Right resp -> eitherDecode (LBSC.pack resp) >>= hits
-        where hits :: Value -> Either Error [Result]
-              hits =
-                parseEither $
-                withArray "suggestions" $ \s -> mapM hit (toList s)
-              hit :: Value -> Parser Result
-              hit =
-                withObject "hit" $ \o -> do
-                  title <- o .: "label"
-                  slug <- o .: "url"
-                  return $
-                    Result
-                      title
-                      Nothing
-                      (Just $ "https://play.tv2.dk" ++ slug)
-                      Nothing
-                      True
-                      providerName
+  tv2Data <- response
+  return . prefixError (providerName ++ ":\n") $
+    (responseBody' <$> tv2Data) >>= decodeTV2
   where
-    tv2 = eitherGetWith opts url
+    response = eitherGetWith opts url
     url =
       "https://r7.tv2.dk/api/3/search/autocomplete/ApplicationCode-Play_Web/query-" ++
       urlEncode' q ++ ".json"
     -- url = "http://localhost:1234/tv2.json?" ++ urlEncode' q
     opts = WWW.defaults & WWW.header "User-Agent" .~ []
-    tv2Resp = unpackResponse <$> tv2
